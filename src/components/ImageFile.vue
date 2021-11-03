bee3f956b6fe93b5d5a8ca40d3c2a1ad4a087a14
HY CHOI <chlghdud17@gmail.com>
2021-11-03 11:55:50 +0900

    imageUPLOAD

---
+++ src/components/ImageFile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="content">
+    <b-form-file
+        v-model="file1"
+        :state="Boolean(file1)"
+        placeholder="Choose a file or drop it here..."
+        drop-placeholder="Drop file here..."
+    ></b-form-file>
+    <b-button @click="viewFile">파일 확인</b-button>
+    <img class="popupImageItem" :src="uploadImageFile" alt="이미지업로드" style="width: 300px; height: 300px;">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageFile",
+  data : ()=> ({
+    file1 : null,
+    uploadImageFile:'',
+
+  }),
+  methods : {
+    viewFile(){
+      console.dir(this.file1)
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.uploadImageFile = e.target.result;
+      }
+      reader.readAsDataURL(this.file1);
+    }
+
+  },
+  mounted() {
+    console.log(this.file1)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="isMap">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <KakaoMap msg="Welcome to Your Vue.js App"/>
+    </div>
+    <div v-else>
+      <ImageFile/>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/KakaoMap.vue'
+import KakaoMap from '@/components/KakaoMap.vue'
+import ImageFile from '@/components/ImageFile.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    KakaoMap,
+    ImageFile
+  },
+  data: () => ({
+    isMap : false
+  }),
 }
 </script>
